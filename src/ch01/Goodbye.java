1618a78eb0702a874dca48379a819ba9e26b6e92
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2024-06-28 20:50:28 -0300

    some ideas about instruction print

---
+++ src/ch01/Goodbye.java
@@ -0,0 +1,23 @@
+package ch01;
+
+// in java, we need to have some mandatory spaces, so, the command publicclassGoodbye will never work
+public class Goodbye {
+    public static void main(String[] args) {
+        // because I use print, the phrase don't break at end of line
+        System.out.print("Goodbye, ");
+        System.out.println("cruel world!");
+    }
+}
+
+// but we have optional spaces, so this two it will work
+
+//public class Goodbye {
+//public static void main(String[] args) {
+//because I use print, the phrase don't break at end of line
+//System.out.print("Goodbye, ");
+//System.out.println("cruel world!");
+//}
+//}
+
+//public class Goodbye { public static void main(String[] args) { System.out.print("Goodbye, "); System.out.println("cruel world!"); } }
+// It's not a good idea to use this king of indentation, because it's hard to read, so it's more difficulty to find bugs and errors
---
+++ src/ch01/HelloWorld3.java
@@ -0,0 +1,19 @@
+package ch01;
+
+public class HelloWorld3 {
+    public static void main(String[] args) {
+        // its possible use escape sequences to make multiple lines with the escape sequence "\n"
+        System.out.print("Hello!\nHow are you?\n");
+
+        // another escape sequences
+
+        // tab "\t"
+        System.out.println("\tHello World!");
+
+        // double quote '\"'
+        System.out.println("She said \"hello\" to me!");
+
+        // blackslash
+        System.out.println("You need to use \\n to break a line!");
+    }
+}
