3ccd3903c53e3f0c8d63007526bd030b26396318
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2024-06-28 18:54:11 -0300

    First two Hello World

---
+++ src/ch01/HelloWorld.java
@@ -0,0 +1,17 @@
+package ch01;
+
+// this is called class, and it's a sequence of methods
+// it's good a class start with a capital letter
+// must have the same file name, so file and class are called HelloWorld
+public class HelloWorld {
+
+    // this is called method, and it's a sequence of instructions
+    // a main method in java it's especial, because every program starts with it and end with it
+    public static void main(String[] args) {
+
+        // generates a simple output
+        // this line is an instruction, more specific, a print instruction
+        System.out.println("Hello World!");
+        // java is case-sensitive, so System != system != SYSTEM
+    }
+}
---
+++ src/ch01/HelloWorld2.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        // generates two simple outputs
+        // you can add a comment at end of line
+        // the phrase between quotation marks, are called string.
+            // the strings are a sequence of characters chained in memory and can be letters, numbers, punctuation and another things
+        // this instruction add especial character at end of line, called new line "\n", to not used, you need to use print instead println
+        System.out.println("Hello World!"); // first line
+        System.out.println("How are you?"); // second line
+    }
+}
