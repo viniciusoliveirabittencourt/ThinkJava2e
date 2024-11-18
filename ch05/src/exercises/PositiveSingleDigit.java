70c8df9a2e8a088185d7ca3aab5a48bba7d99c60
Vinicius Oliveira Bittencourt <viniciusbittencourt2003@gmail.com>
2024-11-18 19:42:41 -0300

    Exercise 5.1

---
+++ ch05/src/exercises/PositiveSingleDigit.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class PositiveSingleDigit {
+    public static void main(String[] args) {
+        int x = 11;
+
+        if (x > 0 && x < 10) {
+            System.out.println("Positive single digit number.");
+        }
+    }
+}
