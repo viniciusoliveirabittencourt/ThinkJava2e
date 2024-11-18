bed9ecc2c31505c70a0c4a07d0f833ee72bcf74d
Vinicius Oliveira Bittencourt <viniciusbittencourt2003@gmail.com>
2024-11-18 14:33:53 -0300

    Chapter 1

--- ch01/src/exercises/HelloWorld.java
+++ ch01/src/exercises/HelloWorld.java
@@ -1,4 +1,9 @@
 package exercises;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        // My comment
+        System.out.println("Hello World");
+        System.out.println("How are you ?");
+    }
 }
