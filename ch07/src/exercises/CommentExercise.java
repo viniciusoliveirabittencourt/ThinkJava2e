409bdeefb199bacfae75be59db9f74c9695299c7
Vinicius Oliveira Bittencourt <viniciusbittencourt2003@gmail.com>
2024-11-25 18:15:53 -0300

    Comment Exercise, exercise 7.1

---
+++ ch07/src/exercises/CommentExercise.java
@@ -0,0 +1,35 @@
+package exercises;
+
+public class CommentExercise {
+    // Sum reducer how only sum the wanted number
+    public static int pineapple(int[] a, int apple) {
+        int pear = 0;
+        for (int pine: a) {
+            if (pine == apple) {
+                pear++;
+            }
+        }
+        return pear;
+    }
+
+    // Multiple reducer operator
+    public static int banana(int[] a) {
+        int kiwi = 1;
+        int i = 0;
+        while (i < a.length) {
+            kiwi = kiwi * a[i];
+            i++;
+        }
+        return kiwi;
+    }
+
+    // Search algorithm
+    public static int grapefruit(int[] a, int grape) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == grape) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
